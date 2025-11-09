12ab499e2b037d01eb2fa8a19b4eda56eda776d6
hideaki-marker-admin <sunday.slowlife@gmail.com>
2025-11-10 00:37:55 +0900

    ヘッダーのレイアウト調整

--- src/components/common/Header.tsx
+++ src/components/common/Header.tsx
@@ -6,13 +6,13 @@ export default function Header() {
     <nav className="fixed top-0 left-0 right-0 z-50 bg-white shadow-lg h-16">
       <div className="container mx-auto flex items-center justify-between h-full">
         {/* レストラン名を左寄せに */}
-        <Link href="/" className="text-2xl font-bold text-gray-800">
+        <Link href="/" className="text-2xl font-bold text-gray-800 whitespace-nowrap flex-shrink-0">
           Restaurant italy
         </Link>
         
         {/* リンク群を中央に配置するコンテナ */}
-        <div className="flex-1 flex justify-center items-center flex-1">
-          <ul className="flex items-center space-x-12 mx-auto">
+        <div className="flex-1 flex justify-center items-center">
+          <ul className="flex items-center gap-20">
             <li>
               <Link href="/showMenu" className="flex items-center text-gray-600 hover:text-gray-900">
                 <Utensils className="h-5 w-5 mr-1" />
