fd4e6e7acbb05f370eae71f0459d2eeb0b4aafd2
hideaki-marker-admin <sunday.slowlife@gmail.com>
2025-11-17 23:54:50 +0900

    ヘッダーのレイアウト修正

--- src/components/common/Header.tsx
+++ src/components/common/Header.tsx
@@ -3,36 +3,38 @@ import { Utensils, LogIn, UserPlus } from 'lucide-react';
 
 export default function Header() {
   return (
-    <nav className="fixed top-0 left-0 right-0 z-50 bg-white shadow-lg h-16">
+    // fixed で全幅を確保
+    <nav className="bg-white shadow-lg">
+      
+      {/* 修正ポイント: containerを使いつつ、w-full flex-nowrap を追加して左右を分離 */}
       <div className="container mx-auto flex items-center justify-between h-full">
-        {/* レストラン名を左寄せに */}
+        
+        {/* レストラン名 (左) */}
         <Link href="/" className="text-2xl font-bold text-gray-800 whitespace-nowrap flex-shrink-0">
           Restaurant italy
         </Link>
         
-        {/* リンク群を中央に配置するコンテナ */}
-        <div className="flex-1 flex justify-center items-center">
-          <ul className="flex items-center gap-20">
-            <li>
-              <Link href="/showMenu" className="flex items-center text-gray-600 hover:text-gray-900">
-                <Utensils className="h-5 w-5 mr-1" />
-                メニュー紹介
-              </Link>
-            </li>
-            <li>
-              <Link href="/login" className="flex items-center text-gray-600 hover:text-gray-900">
-                <LogIn className="h-5 w-5 mr-1" />
-                ログイン
-              </Link>
-            </li>
-            <li>
-              <Link href="/userInsert" className="flex items-center text-gray-600 hover:text-gray-900">
-                <UserPlus className="h-5 w-5 mr-1" />
-                新規お客様登録
-              </Link>
-            </li>
-          </ul>
-        </div>
+        {/* リンク群 (右) */}
+        <ul className="flex items-center space-x-6 list-none flex-shrink-0">
+          <li>
+            <Link href="/showMenu" className="flex items-center text-gray-600 hover:text-gray-900 whitespace-nowrap">
+              <Utensils className="h-5 w-5 mr-1" />
+              メニュー紹介
+            </Link>
+          </li>
+          <li>
+            <Link href="/login" className="flex items-center text-gray-600 hover:text-gray-900 whitespace-nowrap">
+              <LogIn className="h-5 w-5 mr-1" />
+              ログイン
+            </Link>
+          </li>
+          <li>
+            <Link href="/userInsert" className="flex items-center text-gray-600 hover:text-gray-900 whitespace-nowrap">
+              <UserPlus className="h-5 w-5 mr-1" />
+              新規お客様登録
+            </Link>
+          </li>
+        </ul>
       </div>
     </nav>
   );
