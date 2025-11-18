c44b4b986447eb3a0b5f1b7c59544d38ec18b6be
hideaki-marker-admin <sunday.slowlife@gmail.com>
2025-11-19 00:22:28 +0900

    コメント削除

--- src/components/common/AdminBar.tsx
+++ src/components/common/AdminBar.tsx
@@ -1,9 +1,8 @@
 import Link from 'next/link';
-import { UserCog } from 'lucide-react';
 
 export default function AdminBar() {
     return (
-        // ★★★ 修正: fixed bottom-0 left-0 right-0 を追加 ★★★
+        
        <footer className="p-2 !bg-gray-900 !text-white shadow-lg w-full h-10">
             <div className="container mx-auto flex justify-start">
                 <Link href="/adminLogin" className="flex items-center text-sm hover:text-gray-300">
