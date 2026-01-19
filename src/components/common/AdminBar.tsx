d78da3e59578538b6eb0bf2c6a6248cee951d255
hideaki-marker <sunday.slowlife@gmail.com>
2026-01-20 00:17:01 +0900

    フッターの微修正

--- src/components/common/AdminBar.tsx
+++ src/components/common/AdminBar.tsx
@@ -3,9 +3,9 @@ import Link from 'next/link';
 export default function AdminBar() {
     return (
         
-       <footer className="p-2 !bg-gray-900 !text-white shadow-lg w-full h-10">
-            <div className="container mx-auto flex justify-start">
-                <Link href="/adminLogin" className="flex items-center text-sm hover:text-gray-300">
+       <footer className="p-2 !bg-gray-900 !text-white shadow-lg w-full h-16">
+            <div className="container mx-auto flex justify-end items-center h-full px-6">
+                <Link href="/adminLogin" className="text-base hover:text-gray-500 font-medium">
                     管理者ログイン
                 </Link>
             </div>
