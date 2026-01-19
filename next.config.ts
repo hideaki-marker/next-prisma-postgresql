0b1cd322469f8ac78f073e88771f2e9d2d59ed86
hideaki-marker <sunday.slowlife@gmail.com>
2026-01-19 23:42:27 +0900

    スタティック・ルート・インジケーター(左下のNのアイコン)の非表示設定

--- next.config.ts
+++ next.config.ts
@@ -2,6 +2,9 @@ import type { NextConfig } from "next";
 
 const nextConfig: NextConfig = {
   /* config options here */
+  devIndicators: {
+    appIsrStatus: false, // これで左上の「N」アイコンが消えます
+  },
 };
 
 export default nextConfig;
