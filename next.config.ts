22fd6378d02e1522a40b8f54fb3ee59ee98b2048
hideaki-marker <sunday.slowlife@gmail.com>
2026-02-11 00:54:39 +0900

    minIoの画像表示、画像アップロード設定

--- next.config.ts
+++ next.config.ts
@@ -5,6 +5,22 @@ const nextConfig: NextConfig = {
   devIndicators: {
     appIsrStatus: false, // これで左上の「N」アイコンが消えます
   },
+  experimental: {
+    serverActions: {
+      bodySizeLimit: "10mb", // 1MB制限を10MBに引き上げ
+    },
+  },
+  //minIoの画像表示の設定
+  images: {
+    remotePatterns: [
+      {
+        protocol: "http",
+        hostname: "localhost",
+        port: "9000",
+        pathname: "/restaurant-photos/**",
+      },
+    ],
+  },
 };
 
 export default nextConfig;
